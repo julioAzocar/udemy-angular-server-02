ffdceae87be4ae421991ae8e8f4ffd6ad79ebdb7
JULIO CESAR AZOCAR MANSILLA <zf701299@tc2.tech>
2022-10-04 15:02:25 -0300

    project angular de cliente socket inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+//socket 
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = { 
+  url: environment.wsUrl, options: {} 
+};
+
+import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { FooterComponent } from './components/footer/footer.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    SocketIoModule.forRoot(config)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
