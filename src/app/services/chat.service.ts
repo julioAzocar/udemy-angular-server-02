e85e04175a58256dbbd50a45e429c6aed1f24fb3
JULIO CESAR AZOCAR MANSILLA <zf701299@tc2.tech>
2022-10-20 11:55:23 -0300

    seccion 56 terminada

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -33,5 +33,12 @@ export class ChatService {
     return this.wsService.listen('mensaje-privado');
   }
 
+  getUsuariosActivos(){
+    return this.wsService.listen('usuarios-activos');
+  }
+
+  emitirUsuariosActivos(){
+    return this.wsService.emit('obtener-usuarios');
+  }
 
 }
--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Socket } from 'ngx-socket-io';
 import { Usuario } from '../classes/usuario';
 
@@ -10,7 +11,9 @@ export class WebsocketService {
   public socketStatus = false;
   public usuario: Usuario = null;
 
-  constructor(private socket: Socket) { 
+  constructor(
+    private socket: Socket,
+    private router: Router) { 
 
     this.cargarStorage();
     this.checkStatus();
@@ -21,6 +24,7 @@ export class WebsocketService {
     this.socket.on('connect' , () => {
       console.log('Conectado al servidor');
       this.socketStatus = true;
+      this.cargarStorage();
     });
 
     this.socket.on('disconnect' , () => {
@@ -58,6 +62,20 @@ export class WebsocketService {
 
       });
     });
+
+  }
+
+  logoutWS(){
+    this.usuario = null;
+    localStorage.removeItem('usuario');
+
+    const payload = {
+      nombre: 'sin-nombre'
+    };
+
+//() => {}   calback funcion cualquiera
+    this.emit('configurar-usuario', payload, () => {}) ;
+    this.router.navigateByUrl('');
 
   }
 
